2ac916090a9edfe506adb0d419fa7d0ff3628d72
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-03-31 18:05:16 -0400

    Iterative python solution

---
+++ python/iterative.py
@@ -0,0 +1,22 @@
+# Great, kid. Don't get cocky. https://www.youtube.com/watch?v=nN9xsFUsPqM
+
+def ackermann(m, n):
+    stack = []
+    stack.append(m)
+    while len(stack):
+        m = stack.pop()
+        if m == 0:
+            n = n + 1
+        elif n == 0:
+            n = 1
+            stack.append(m-1)
+        else:
+            n = n - 1
+            stack.append(m-1)
+            stack.append(m)
+    return n
+
+if __name__ == "__main__":
+    m, n = map(int, raw_input("Enter m and n: ").split())
+    r = ackermann(m, n)
+    print "Result:", r
