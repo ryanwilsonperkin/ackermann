4ef4f35f91fd9f063425c8bdba183d9ee6b5334f
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-03-31 14:47:31 -0400

    Add provided ada recursive version.

---
+++ ada/recursive.adb
@@ -0,0 +1,29 @@
+with Ada.Text_IO;  use Ada.Text_IO;
+with ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure recursive is
+
+    function Ackermann (M, N : Natural) return Natural is
+    begin
+        if M = 0 then
+               return N + 1;
+        elsif N = 0 then
+               return Ackermann (M - 1, 1);
+        else
+               return Ackermann (M - 1, Ackermann (M, N - 1));
+        end if;
+    end Ackermann;
+
+begin
+    for M in 0..3 loop
+       for N in 0..6 loop
+           Put(M);
+           Put(N);
+           Put(": ");
+           Put(Natural'Image (Ackermann (M, N)));
+           New_Line;
+       end loop;
+       New_Line;
+    end loop;
+end recursive;
+
